5a1fb42358d065c7b2f39edee7d51295c054d815
Luke Snowden <luke@purposemedia.co.uk>
2014-07-15 14:25:08 +0100

    Added in new Booststrap style

--- src/LukeSnowden/Menu/Styles/Styles.php
+++ src/LukeSnowden/Menu/Styles/Styles.php
@@ -2,22 +2,40 @@
 
 class Styles
 {
-
-	public static function renderHorizontal( $structure = array(), $depth = 1 )
+	/*
+	public static function renderHorzontal( $structure = array(), $depth = 1 )
 	{
-		if( $depth === 1 ) ob_start();
 		?>
 		<?php foreach( $structure as $level ) : ?>
 			<li class="<?php echo $level['class']; ?> <?php echo $level['parent'] === false ? 'container' : 'nav-node'; ?> node-<?php echo $depth; ?>">
 				<a href="<?php echo $level['URL']; ?>"><?php echo $level['text']; ?></a>
 				<?php if( ! empty( $level['children'] ) ) : ?>
 					<ul>
-						<?php self::renderHorizontal( $level['children'], ($depth+1) ); ?>
+						<?php self::renderHorzontal( $level['children'], ($depth+1) ); ?>
 					</ul>
 				<?php endif; ?>
 			</li>
 		<?php endforeach; ?>
 		<?php
+	}
+	*/
+
+	public static function renderNavTabsDropdowns( $structure = array(), $depth = 1 )
+	{
+		if( $depth === 1 ) ob_start();
+		foreach( $structure as $level ) :
+			$class = preg_replace( '/current/', 'active', $level['class'] );
+			echo '<li class=" ' . $class . ' ' . ( empty( $level['children'] ) ? '' : 'dropdown' ) . '">';
+				if( ! empty( $level['children'] ) ) :
+					echo '<a href="#" class="dropdown-toggle" data-toggle="dropdown">' . $level['text'] . ' <span class="caret"></span></a>';
+					echo '<ul class="dropdown-menu" role="menu">';
+						echo self::renderNavTabsDropdowns( $level['children'], ($depth+1) );
+					echo '</ul>';
+				else :
+					echo '<a href="' . $level['URL'] . '">' . $level['text'] . '</a>';
+				endif;
+			echo '</li>';
+		endforeach;
 		if( $depth === 1 ) return ob_get_clean();
 	}
 
