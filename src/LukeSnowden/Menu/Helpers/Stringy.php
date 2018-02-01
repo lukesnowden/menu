f49bc876155158e998f854915045847b12c82ff0
Daniel Watson <daniel.watson@purposemedia.co.uk>
2018-02-01 15:00:05 +0000

    Removed deprecated create_function usage (PHP 7.2)

--- src/LukeSnowden/Menu/Helpers/Stringy.php
+++ src/LukeSnowden/Menu/Helpers/Stringy.php
@@ -13,7 +13,9 @@ class Stringy {
 		if( $capitalise_first_char ) {
 			$str[0] = strtoupper( $str[0] );
 		}
-		$func = create_function( '$c', 'return strtoupper($c[1]);' );
+        $func = function($c) {
+            return strtoupper($c[1]);
+        };
 		return preg_replace_callback( '/_|-([a-z])/', $func, $str );
 	}
 
